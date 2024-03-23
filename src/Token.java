c3d39e3ea0aad8ed4b6f5005a419cf482bea6f57
Óskar Jörgen Sandholt <oskarjorgen@gmail.com>
2024-03-23 22:33:29 +0000

    Chapter 2.4

--- src/Token.java
+++ src/Token.java
@@ -4,14 +4,14 @@ public class Token {
     final Object literal;
     final int line;
 
-   Token(TokenType type, String lexeme, Object literal, int line) {
-       this.type = type;
-       this.lexeme = lexeme;
-       this.literal = literal;
-       this.line = line;
-   }
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
 
-   public String toString() {
-       return STR."\{type} \{lexeme} \{literal}";
-   }
+    public String toString() {
+        return STR."\{type} \{lexeme} \{literal}";
+    }
 }
