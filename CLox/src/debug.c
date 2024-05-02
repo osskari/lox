27aaebdae414f6df7be63d0089ccd0f736bc8a87
Óskar Jörgen Sandholt <oskar.j.sandholt@origo.is>
2024-05-02 13:41:49 +0000

    Chapter 21.3 done

--- CLox/src/debug.c
+++ CLox/src/debug.c
@@ -42,12 +42,20 @@ int disassembleIntruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_TRUE", offset);
   case OP_FALSE:
     return simpleInstruction("OP_FALSE", offset);
+  case OP_POP:
+    return simpleInstruction("OP_POP", offset);
+  case OP_GET_GLOBAL:
+    return simpleInstruction("OP_GET_GLOBAL", offset);
+  case OP_DEFINE_GLOBAL:
+    return simpleInstruction("OP_DEFINE_GLOBAL", offset);
   case OP_EQUAL:
     return simpleInstruction("OP_EQUAL", offset);
   case OP_GREATER:
     return simpleInstruction("OP_GREATER", offset);
   case OP_LESS:
     return simpleInstruction("OP_LESS", offset);
+  case OP_PRINT:
+    return simpleInstruction("OP_PRINT", offset);
   case OP_RETURN:
     return simpleInstruction("OP_RETURN", offset);
   case OP_ADD:
