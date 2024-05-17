11cf556d5a105177965d32ba40b160aa152fb0c1
Óskar Jörgen Sandholt <oskar.j.sandholt@origo.is>
2024-05-17 13:10:41 +0000

    Chapter 25 done

--- CLox/src/debug.c
+++ CLox/src/debug.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "object.h"
 #include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name) {
@@ -68,6 +69,10 @@ int disassembleIntruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_DEFINE_GLOBAL", offset);
   case OP_SET_GLOBAL:
     return simpleInstruction("OP_SET_GLOBAL", offset);
+  case OP_GET_UPVALUE:
+    return byteInstruction("OP_GET_UPVALUE", chunk, offset);
+  case OP_SET_UPVALUE:
+    return byteInstruction("OP_SET_UPVALUE", chunk, offset);
   case OP_EQUAL:
     return simpleInstruction("OP_EQUAL", offset);
   case OP_GREATER:
@@ -84,6 +89,23 @@ int disassembleIntruction(Chunk *chunk, int offset) {
     return jumpInstruction("OP_LOOP", -1, chunk, offset);
   case OP_CALL:
     return byteInstruction("OP_CALL", chunk, offset);
+  case OP_CLOSURE:
+    offset++;
+    uint8_t constant = chunk->code[offset++];
+    printf("%-16s %4d ", "OP_CLOSURE", constant);
+    printValue(chunk->constants.values[constant]);
+    printf("\n");
+
+    ObjFunction *function = AS_FUNCTION(chunk->constants.values[constant]);
+    for (int j = 0; j < function->upvalueCount; j++) {
+      int isLocal = chunk->code[offset++];
+      int index = chunk->code[offset++];
+      printf("%04d  |   %s %d\n", offset - 2, isLocal ? "Local" : "upvalue",
+             index);
+    }
+    return offset;
+  case OP_CLOSE_UPVALUE:
+    return simpleInstruction("OP_CLOSE_UPVALUE", offset);
   case OP_RETURN:
     return simpleInstruction("OP_RETURN", offset);
   case OP_ADD:
