4cd358ffed748b882a9186acadae16c3a9df0f4d
Óskar Jörgen Sandholt <oskar.j.sandholt@origo.is>
2024-05-27 11:10:28 +0000

    Chapter 28 done

--- CLox/src/memory.c
+++ CLox/src/memory.c
@@ -49,6 +49,7 @@ static void markRoots() {
 
   markTable(&vm.globals);
   markCompilerRoots();
+  markObject((Obj *)vm.initString);
 }
 
 static void markArray(ValueArray *array) {
@@ -65,9 +66,16 @@ static void blackenObject(Obj *object) {
 #endif
 
   switch (object->type) {
+  case OBJ_BOUND_METHOD: {
+    ObjBoundMethod *bound = (ObjBoundMethod *)object;
+    markValue(bound->receiver);
+    markObject((Obj *)bound->method);
+    break;
+  }
   case OBJ_CLASS: {
     ObjClass *klass = (ObjClass *)object;
     markObject((Obj *)klass->name);
+    markTable(&klass->methods);
     break;
   }
   case OBJ_CLOSURE: {
@@ -111,9 +119,15 @@ static void freeObject(Obj *object) {
   printf("%p free type %d\n", (void *)object, object->type);
 #endif
   switch (object->type) {
-  case OBJ_CLASS:
+  case OBJ_BOUND_METHOD:
+    FREE(ObjBoundMethod, object);
+    break;
+  case OBJ_CLASS: {
+    ObjClass *klass = (ObjClass *)object;
+    freeTable(&klass->methods);
     FREE(ObjClass, object);
     break;
+  }
   case OBJ_CLOSURE: {
     ObjClosure *closure = (ObjClosure *)object;
     FREE_ARRAY(ObjUpvalue *, closure->upvalues, closure->upvalueCount);
